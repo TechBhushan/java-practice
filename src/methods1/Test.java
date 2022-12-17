fe9d1dea3a7d62bc4e254c3907afdbb46ecc3368
TechBhushan <techbhushan357@gmail.com>
2022-12-17 14:42:35 +0530

    edited

--- src/methods1/Test.java
+++ src/methods1/Test.java
@@ -8,15 +8,12 @@ public class Test {
 	public double getBalance() {
 		return balance;
 	}
-
 	public void setBalance(double balance) {
 		this.balance = this.balance+balance;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
